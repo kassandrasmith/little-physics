a76c0c3ef7df4f65c887ed1c6484d98045be2c83
kassandrasmith <kassandrasmith@utexas.edu>
2016-04-12 22:14:42 -0500

    Create GUI add stubs

---
+++ src/InputConditions.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+/**
+ * Created by kassandra on 4/12/16.
+ */
+public class InputConditions {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+}
--- src/Velocity.java
+++ src/Velocity.java
@@ -1,6 +1,21 @@
 
 public class Velocity {
-    int v0 = 0;
+    int initialVelocity = 0;
     int velocity = 0;
+    int[] velocityTable = new int[100];
+
+    public void calculateVelocity(int acceleration){
+        for(int t=0; t<100; t++) {
+            velocity = initialVelocity + (acceleration * t);
+            velocityTable[t] = velocity;
+        }
+    }
+    public int calculateVelocityAtTime(int acceleration, int time){
+        for(int t=0; t<time; t++) {
+            velocity = initialVelocity + (acceleration * t);
+            velocityTable[t] = velocity;
+        }
+        return initialVelocity +(acceleration * time);
+    }
 
 }
