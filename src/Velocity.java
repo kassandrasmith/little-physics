4409854f3a0b48aec80f3a3094b6c82f59d85ea7
kassandrasmith <kassandrasmith@utexas.edu>
2016-04-20 23:26:04 -0500

    Add calculations Remove JFrame attempts Remove starter stubs Update gitignore

--- src/Velocity.java
+++ src/Velocity.java
@@ -1,16 +1,27 @@
 
 public class Velocity {
-    int initialVelocity = 0;
-    int velocity = 0;
-    int[] velocityTable = new int[100];
+    double initialVelocity = 0;
+    double velocity = 0;
+    double[] velocityTable = new double[100];
     boolean checked = false;
+    double acceleration;
+    double time;
+
+    public double calculateVelocity(){
+        for(int t=0; t<100; t++) {
+            velocity = initialVelocity + (acceleration * t);
+            velocityTable[t] = velocity;
+        }
+        return initialVelocity +(acceleration * time);
+    }
+
     public void calculateVelocity(int acceleration){
         for(int t=0; t<100; t++) {
             velocity = initialVelocity + (acceleration * t);
             velocityTable[t] = velocity;
         }
     }
-    public int calculateVelocityAtTime(int acceleration, int time){
+    public double calculateVelocityAtTime(int acceleration, int time){
         for(int t=0; t<time; t++) {
             velocity = initialVelocity + (acceleration * t);
             velocityTable[t] = velocity;
