35d8ccb38aa5e83d8a132c871dc9ea79302f170a
kassandrasmith <kassandrasmith@utexas.edu>
2016-04-24 18:23:51 -0500

    Make calculations work

--- src/physics/Opener.java
+++ src/physics/Opener.java
@@ -1,68 +1,69 @@
-
-package physics;
-
-import javax.swing.*;
-import java.awt.event.*;
-
-public class Opener extends JDialog {
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
-
-    public Opener() {
-        setContentPane(contentPane);
-        setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
-
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-
-// call onCancel() when cross is clicked
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-// call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-    }
-
-    private void onOK() {
-        JFrame frame = new JFrame("PhysicsGUI");
-        frame.setContentPane(new PhysicsGUI().physicsPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-// add your code here
-
-
-        dispose();
-    }
-
-    private void onCancel() {
-// add your code here if necessary
-        dispose();
-    }
-
-    public static void main(String[] args) {
-        Opener dialog = new Opener();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
-    }
-}
+//
+//package physics;
+//
+//import javax.swing.*;
+//import java.awt.event.*;
+//
+//public class Opener extends JDialog {
+//    private JPanel contentPane;
+//    private JButton buttonOK;
+//    private JButton buttonCancel;
+//
+//    public Opener() {
+//        setContentPane(contentPane);
+//        setModal(true);
+//        getRootPane().setDefaultButton(buttonOK);
+//
+//        buttonOK.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                onOK();
+//            }
+//        });
+//
+//        buttonCancel.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                onCancel();
+//            }
+//        });
+//
+//// call onCancel() when cross is clicked
+//        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+//        addWindowListener(new WindowAdapter() {
+//            public void windowClosing(WindowEvent e) {
+//                onCancel();
+//            }
+//        });
+//
+//// call onCancel() on ESCAPE
+//        contentPane.registerKeyboardAction(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                onCancel();
+//            }
+//        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+//    }
+//
+//    private void onOK() {
+//        JFrame frame = new JFrame("PhysicsGUI");
+//        frame.setContentPane(new PhysicsGUI().physicsPanel);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//// add your code here
+//
+//
+//        dispose();
+//    }
+//
+//    private void onCancel() {
+//// add your code here if necessary
+//        dispose();
+//    }
+//
+//    public static void main(String[] args) {
+//        Opener dialog = new Opener();
+//        dialog.pack();
+//        dialog.setVisible(true);
+//        System.exit(0);
+//    }
+//
+//}
