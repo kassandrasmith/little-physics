fadb5cc63d34f31e65a6cf8cae670bd1bb4dfff6
kassandrasmith <kassandrasmith@utexas.edu>
2016-04-30 13:05:27 -0500

    Add method for getting numbers from form

--- src/physics/GetNumbers.java
+++ src/physics/GetNumbers.java
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 package physics;
 
+=======
+>>>>>>> ade0834... Use JPanels instead of forms
+=======
+package physics;
+
+>>>>>>> a74c744... Make calculations work
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -7,6 +15,10 @@ public class GetNumbers extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a74c744... Make calculations work
     private JTextField initialVelocity;
     private JTextField acceleration;
     private JTextField inputTime;
@@ -14,6 +26,17 @@ public class GetNumbers extends JDialog {
     private JTextField initialX;
     private JTextField outputx;
     public Velocity calculator;
+<<<<<<< HEAD
+=======
+    private JTextField TextField1;
+    private JTextField TextField2;
+    private JTextField TextField3;
+    private JTextField textField4;
+    private JTextField TextField5;
+    private JTextField textField6;
+>>>>>>> ade0834... Use JPanels instead of forms
+=======
+>>>>>>> a74c744... Make calculations work
 
     public GetNumbers() {
         setContentPane(contentPane);
@@ -48,6 +71,9 @@ public class GetNumbers extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
+<<<<<<< HEAD
+    private void onOK() #!
+=======
     private void onOK() {
 // add your code here
         calculator = new Velocity();
@@ -60,6 +86,7 @@ public class GetNumbers extends JDialog {
 
 
     }
+>>>>>>> ade0834... Use JPanels instead of forms
 
     private void onCancel() {
 // add your code here if necessary
@@ -72,9 +99,18 @@ public class GetNumbers extends JDialog {
         dialog.setVisible(true);
         System.exit(0);
     }
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a74c744... Make calculations work
 
     private double makeDouble(javax.swing.JTextField input){
         String out = input.getText();
         return Double.parseDouble(out);
     }
+<<<<<<< HEAD
+=======
+>>>>>>> ade0834... Use JPanels instead of forms
+=======
+>>>>>>> a74c744... Make calculations work
 }
