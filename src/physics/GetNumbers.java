d7959d63c573dceb3c792d2013771e6c8e73ab60
kassandrasmith <kassandrasmith@utexas.edu>
2016-05-23 21:35:44 -0500

    Fix merging errors

--- src/physics/GetNumbers.java
+++ src/physics/GetNumbers.java
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package physics;
-
-=======
->>>>>>> ade0834... Use JPanels instead of forms
-=======
-package physics;
-
->>>>>>> a74c744... Make calculations work
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -15,10 +6,6 @@ public class GetNumbers extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a74c744... Make calculations work
     private JTextField initialVelocity;
     private JTextField acceleration;
     private JTextField inputTime;
@@ -26,20 +13,9 @@ public class GetNumbers extends JDialog {
     private JTextField initialX;
     private JTextField outputx;
     public Velocity calculator;
-<<<<<<< HEAD
-=======
-    private JTextField TextField1;
-    private JTextField TextField2;
-    private JTextField TextField3;
-    private JTextField textField4;
-    private JTextField TextField5;
-    private JTextField textField6;
->>>>>>> ade0834... Use JPanels instead of forms
-=======
->>>>>>> a74c744... Make calculations work
-
     public GetNumbers() {
-        setContentPane(contentPane);
+        final JPanel contentPane = this.contentPane;
+        setContentPane(this.contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
@@ -64,45 +40,21 @@ public class GetNumbers extends JDialog {
         });
 
 // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
+        this.contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
-<<<<<<< HEAD
     private void onOK() {
 // add your code here
         calculator = new Velocity();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private void onOK() #!
-=======
-=======
->>>>>>> 491f85d4e461251797a2ebb27842d7dfd59d9d74
-    private void onOK() {
-// add your code here
-        calculator = new Velocity();
-
-=======
->>>>>>> 491f85d... Update number retrieval
         calculator.acceleration = makeDouble(acceleration);
         calculator.initialVelocity = makeDouble(initialVelocity);
         calculator.time = makeDouble(inputTime);
         velocityAtTime.replaceSelection(" " + calculator.calculateVelocity());
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 491f85d4e461251797a2ebb27842d7dfd59d9d74
     }
->>>>>>> ade0834... Use JPanels instead of forms
-=======
-    }
->>>>>>> 491f85d... Update number retrieval
 
     private void onCancel() {
 // add your code here if necessary
@@ -115,18 +67,9 @@ public class GetNumbers extends JDialog {
         dialog.setVisible(true);
         System.exit(0);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a74c744... Make calculations work
 
     private double makeDouble(javax.swing.JTextField input){
         String out = input.getText();
         return Double.parseDouble(out);
     }
-<<<<<<< HEAD
-=======
->>>>>>> ade0834... Use JPanels instead of forms
-=======
->>>>>>> a74c744... Make calculations work
 }
